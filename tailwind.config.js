c76b8de7a61c3782a4bff5b416381959395e12e4
Joris Douven <jdouven@ilionx.com>
2024-04-03 15:26:17 +0200

    build: add tailwind, prettier, eslint

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    fontFamily: {
+      sans: ['Roboto', 'Helvetica Neue', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
